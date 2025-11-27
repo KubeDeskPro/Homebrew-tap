8ff727725011c3d671e083bf6c481d6f3a36b3c2
Mahdi Torabi <torabi89@gmail.com>
2025-11-27 14:41:24 -0800

    Release KubeDesk Helper 2.1.5

--- Formula/kubedesk-helper.rb
+++ Formula/kubedesk-helper.rb
@@ -11,9 +11,9 @@
 class KubedeskHelper < Formula
   desc "Helper service for KubeDesk - Kubernetes management for macOS"
   homepage "https://github.com/kubedeskpro/kubedesk-helper"
-  url "https://github.com/kubedeskpro/kubedesk-helper/releases/download/v2.1.4/kubedesk-helper-2.1.4.tar.gz"
-  sha256 "4e158588ea4c896480a9700e2fa716d00658e6702aa125249c6d4ef71aff2abf"
-  version "2.1.4"
+  url "https://github.com/kubedeskpro/kubedesk-helper/releases/download/v2.1.5/kubedesk-helper-2.1.5.tar.gz"
+  sha256 "558f34108998a8bf0e563d4bce09d52bbb4a4f87588c1831aec62261cd9730d2"
+  version "2.1.5"
   license "MIT"
 
   depends_on :macos
@@ -36,7 +36,7 @@ class KubedeskHelper < Formula
 
   def caveats
     <<~EOS
-      KubeDesk Helper v2.1.4 has been installed!
+      KubeDesk Helper v2.1.5 has been installed!
 
       To start the helper service now and restart at login:
         brew services start kubedesk-helper
@@ -69,7 +69,7 @@ class KubedeskHelper < Formula
 
     # Test health endpoint
     output = shell_output("curl -s http://localhost:47823/health")
-    assert_match "2.1.4", output
+    assert_match "2.1.5", output
     assert_match "ok", output
 
     # Clean up
